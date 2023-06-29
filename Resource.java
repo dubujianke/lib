afaac9930492fa71ce4aa59479d404fdbaa232b1
dubujianke <wukong332@126.com>
2023-06-29 18:27:43 +0800

    11
    11111

---
+++ Resource.java
@@ -0,0 +1,7 @@
+package com.mobile.sdk.configuration;
+
+/* loaded from: param-1.1.3.aar:classes.jar:com/mobile/sdk/configuration/Resource.class */
+public interface Resource {
+    public static final String EyeFileName = "eye.properties";
+    public static final String FilmFileName = "3d.properties";
+}
